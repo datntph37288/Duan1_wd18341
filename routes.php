47277cfe18140a210ddf6fc07ba6dda9a5de586c
NguyenLeQuyen2004 <quyennlph37238@fpt.edu.vn>
2023-11-20 12:58:26 +0700

    update

--- routes.php
+++ routes.php
@@ -2,16 +2,18 @@
 
 use Ductong\BaseMvc\Controllers\Admin\UserController;
 use Ductong\BaseMvc\Controllers\Admin\CategoryController;
+use Ductong\BaseMvc\Controllers\Admin\DashboardController;
 use Ductong\BaseMvc\Controllers\Client\HomeController;
 use Ductong\BaseMvc\Router;
 
 $router = new Router();
 
 $router->addRoute('/', HomeController::class, 'index');
+$router->addRoute('/admin/dashboard', DashboardController::class, 'index');
 
 $router->addRoute('/admin/users', UserController::class, 'index');
 $router->addRoute('/admin/users/create', UserController::class, 'create');
-$router->addRoute('/adgmin/users/update', UserController::class, 'update');
+$router->addRoute('/admin/users/update', UserController::class, 'update');
 $router->addRoute('/admin/users/delete', UserController::class, 'delete');
 
 $router->addRoute('/admin/categories', CategoryController::class, 'index');
--- src/Controller.php
+++ src/Controller.php
@@ -3,9 +3,16 @@
 namespace Ductong\BaseMvc;
 
 class Controller {
+    // render ra giao dieen ng dung
     protected function render($view, $data = []) {
         extract($data);
 
         include "Views/$view.php";
+    }
+    protected function renderAdmin($view, $data = []) {
+        $data["view"] = $view;
+        extract($data);
+
+        include "Views/admin/master.php";
     }
 }
